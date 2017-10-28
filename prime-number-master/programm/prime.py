5669641a1ccc5def4d7a93827c0ebc5a6927e55f
Nicolaks <32768134+Nicolaks@users.noreply.github.com>
2017-10-28 23:39:36 +0200

    0.0.6
    Add better function to calculate prime number

--- prime-number-master/programm/prime.py
+++ prime-number-master/programm/prime.py
@@ -1,9 +1,10 @@
 # <thanks to https://stackoverflow.com/users/166949/steveha
 import os
+from math import *
 
 # Function return true if a number is prime.
 def is_prime(n):
-	for i in range(2, n):
+	for i in range(2, int(sqrt(n)+1)):
 		if n%i == 0:
 			return False
 	return True
