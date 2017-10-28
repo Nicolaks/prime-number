95775394f31472e6ca34ef49b8fcacd2c55fc4d5
Nicolaks <32768134+Nicolaks@users.noreply.github.com>
2017-10-28 17:58:02 +0200

    0.0.4
    Add comments

--- prime-number-master/programm/prime.py
+++ prime-number-master/programm/prime.py
@@ -1,7 +1,7 @@
 # <thanks to https://stackoverflow.com/users/166949/steveha
 
 
-
+# Function return true if a number is prime.
 def is_prime(n):
 	for i in range(2, n):
 		if n%i == 0:
@@ -9,11 +9,22 @@ def is_prime(n):
 	return True
 
 
-n = int(input("What number should I go up to ? "))
 
-for p in range(2, n+1):
-	if is_prime(p):
-		print(p)
-		fichier = open("data.txt", "a")
-		fichier.write(str(p) + " " + str(is_prime(p)) + "\n")
-		fichier.close()
+# Create a while, with a top number, and check if the number
+# is prime ans after put him in file.
+def launch():
+	n = int(input("What number should I go up to ? "))
+
+	for p in range(2, n+1):
+		if is_prime(p):
+			print(p)
+			prime_file = open("data.txt", "a")
+			prime_file.write(str(p) + " " + str(is_prime(p)) + "\n")
+			prime_file.close()
+
+
+# Main function how launch all the program.
+def main():
+	launch()
+
+main()
